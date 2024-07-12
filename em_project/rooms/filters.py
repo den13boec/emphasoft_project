2780f505ccafe9b2ca005ed6cf31d1814065f69e
den13boec <den-boris@list.ru>
2024-07-12 22:45:47 +0300

    Added custom filters

---
+++ em_project/rooms/filters.py
@@ -0,0 +1,28 @@
+import django_filters
+
+from .models import Booking, Room
+
+
+class RoomFilter(django_filters.FilterSet):
+    min_price = django_filters.NumberFilter(field_name='price_per_night', lookup_expr='gte')
+    max_price = django_filters.NumberFilter(field_name='price_per_night', lookup_expr='lte')
+    min_capacity = django_filters.NumberFilter(field_name='capacity', lookup_expr='gte')
+    max_capacity = django_filters.NumberFilter(field_name='capacity', lookup_expr='lte')
+    start_date = django_filters.DateFilter(label='Start date', method='filter_by_availability')
+    end_date = django_filters.DateFilter(label='End date', method='filter_by_availability')
+
+    class Meta:
+        model = Room
+        fields = ['min_price', 'max_price', 'min_capacity', 'max_capacity', 'start_date', 'end_date']
+
+    def filter_by_availability(self, queryset, name, value):
+        start_date = self.data.get('start_date')
+        end_date = self.data.get('end_date')
+
+        if start_date and end_date:
+            conflicting_bookings = Booking.objects.filter(
+                start_date__lt=end_date, end_date__gt=start_date
+            ).values_list('room_id', flat=True)
+            queryset = queryset.exclude(id__in=conflicting_bookings)
+
+        return queryset
--- em_project/rooms/views.py
+++ em_project/rooms/views.py
@@ -2,6 +2,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
 
+from .filters import RoomFilter
 from .models import Booking, Room
 from .serializers import BookingSerializer, RoomSerializer
 
@@ -10,8 +11,9 @@ class RoomViewSet(ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    filterset_fields = ["price_per_night", "capacity"]
-    ordering_fields = ["price_per_night", "capacity"]
+    filterset_class = RoomFilter
+    ordering_fields = ['price_per_night', 'capacity']
+    ordering = ['price_per_night']
 
 
 class BookingViewSet(ModelViewSet):
